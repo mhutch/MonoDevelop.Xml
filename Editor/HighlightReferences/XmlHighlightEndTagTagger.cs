ab16ca8a90926abfa31699f23fdd9586d08dda86
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-09-09 18:25:06 -0400

    Add matching tag highlighter

---
+++ Editor/HighlightReferences/XmlHighlightEndTagTagger.cs
@@ -0,0 +1,84 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+
+using MonoDevelop.Xml.Dom;
+using MonoDevelop.Xml.Editor.Completion;
+using MonoDevelop.Xml.Editor.HighlightReferences;
+using MonoDevelop.Xml.Parser;
+
+namespace MonoDevelop.MSBuild.Editor.HighlightReferences
+{
+	class XmlHighlightEndTagTagger : HighlightTagger<ITextMarkerTag, ITextMarkerTag>
+	{
+		IXmlBackgroundParser parser;
+
+		public XmlHighlightEndTagTagger (
+			ITextView textView, XmlHighlightEndTagTaggerProvider provider
+			)
+			: base (textView, provider.JoinableTaskContext)
+		{
+			if (!XmlBackgroundParser.TryGetParser (TextView.TextBuffer, out parser)) {
+				throw new InvalidOperationException ("XmlCore TextView does not have an XML parser");
+			}
+			Console.WriteLine (parser);
+		}
+
+		protected async override
+			Task<(SnapshotSpan sourceSpan, ImmutableArray<(ITextMarkerTag kind, SnapshotSpan location)> highlights)>
+			GetHighlightsAsync (SnapshotPoint caretLocation, CancellationToken token)
+		{
+			var spine = parser.GetSpineParser (caretLocation);
+			if (!(spine.CurrentState is XmlNameState) || !(spine.CurrentState.Parent is XmlTagState || spine.CurrentState.Parent is XmlClosingTagState)) {
+				return Empty;
+			}
+
+			var parseResult = await parser.GetOrParseAsync (caretLocation.Snapshot, token);
+			var node = parseResult.XDocument.RootElement.FindNodeAtOffset (caretLocation.Position);
+
+			if (node is XElement element) {
+				if (element.ClosingTag is XClosingTag closingTag && closingTag.IsNamed) {
+					return CreateResult (element.NameSpan, closingTag.NameSpan);
+				}
+			} else if (node is XClosingTag closingTag) {
+				var matchingElement = parseResult.XDocument.AllDescendentNodes
+					.OfType<XElement> ()
+					.FirstOrDefault (t => t.ClosingTag == closingTag);
+				if (matchingElement != null && matchingElement.IsNamed) {
+					return CreateResult (closingTag.NameSpan, matchingElement.NameSpan);
+				}
+			}
+			return Empty;
+
+			(SnapshotSpan sourceSpan, ImmutableArray<(ITextMarkerTag kind, SnapshotSpan location)> highlights)
+				CreateResult (TextSpan source, TextSpan target)
+				=> (
+					new SnapshotSpan (caretLocation.Snapshot, source.Start, source.Length),
+					ImmutableArray<(ITextMarkerTag kind, SnapshotSpan location)>.Empty
+					.Add ((
+						MatchingTagHighlightTag.Instance,
+						new SnapshotSpan (caretLocation.Snapshot, target.Start, target.Length)))
+				);
+		}
+
+		protected override ITextMarkerTag GetTag (ITextMarkerTag kind) => kind;
+	}
+
+	sealed class MatchingTagHighlightTag : TextMarkerTag
+	{
+		internal const string TagId = "brace matching";
+
+		public static readonly MatchingTagHighlightTag Instance = new MatchingTagHighlightTag ();
+
+		private MatchingTagHighlightTag () : base (TagId) { }
+	}
+}
---
+++ Editor/HighlightReferences/XmlHighlightEndTagTaggerProvider.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Threading;
+using Microsoft.VisualStudio.Utilities;
+using MonoDevelop.Xml.Editor;
+using MonoDevelop.Xml.Editor.Tags;
+
+namespace MonoDevelop.MSBuild.Editor.HighlightReferences
+{
+	[Export(typeof(IViewTaggerProvider))]
+	[ContentType (XmlContentTypeNames.XmlCore)]
+	[TagType (typeof (NavigableHighlightTag))]
+	[TextViewRole (PredefinedTextViewRoles.Interactive)]
+	class XmlHighlightEndTagTaggerProvider : IViewTaggerProvider
+	{
+		[Import]
+		public JoinableTaskContext JoinableTaskContext { get; set; }
+
+		public ITagger<T> CreateTagger<T> (ITextView textView, ITextBuffer buffer) where T : ITag
+			=>  (ITagger<T>) buffer.Properties.GetOrCreateSingletonProperty (
+				() => new XmlHighlightEndTagTagger (textView, this)
+			);
+	}
+}
