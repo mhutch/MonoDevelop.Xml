e87231b2772003aa840ca7b5dc71beb7fb75d5de
Kirill Osenkov <github@osenkov.com>
2020-01-10 17:24:05 -0800

    Add VSMac XML addin.

---
+++ VSMac/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin (
+	"MonoDevelop.XmlEditor",
+	Namespace = "MonoDevelop.XmlEditor",
+	Version = "2.3.1"
+)]
+
+[assembly: AddinName ("Xml Editor")]
+[assembly: AddinCategory ("IDE extensions")]
+[assembly: AddinDescription ("Editing support for XML files")]
+[assembly: AddinAuthor ("Mikayla Hutchinson")]
