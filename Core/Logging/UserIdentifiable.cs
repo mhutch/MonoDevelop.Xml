134603b7b4433b5b7c440e773a1f71162bd62cf3
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2023-06-20 14:47:15 -0400

    Simplify user-identifiable strings API

--- Core/Logging/UserIdentifiable.cs
+++ Core/Logging/UserIdentifiable.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#nullable enable
+
 using System;
 
 using Microsoft.Extensions.Logging;
@@ -8,51 +10,44 @@ using Microsoft.Extensions.Logging;
 namespace MonoDevelop.Xml.Logging;
 
 /// <summary>
-/// A value that may contain user-identifiable information and is
-/// intended to be converted to a string and logged to an <see cref="ILogger"/>.
-/// The logger must hash this value if PII is a concern.
+/// A value that may contain user-identifiable information and is intended to be converted
+/// to a string and logged to an <see cref="ILogger"/>.
+/// Loggers that strip PII must hash this value unless it can be reliably determined
+/// to be a non-identifiable value such a type or filename that is part of the app itself.
 /// </summary>
-public struct UserIdentifiable<TValue>
+public interface IUserIdentifiableValue
+{
+	public object? Value { get; }
+}
+
+/// </inheritdoc>
+public interface IUserIdentifiable<TValue> : IUserIdentifiableValue
+{
+	public new TValue Value { get; }
+}
+
+/// </inheritdoc>
+public readonly struct UserIdentifiable<TValue> : IUserIdentifiable<TValue>
 {
 	public TValue Value { get; }
 	public UserIdentifiable (TValue value) { Value = value; }
 	public static implicit operator UserIdentifiable<TValue> (TValue value) => new (value);
-	public override readonly string ToString () => Value?.ToString () ?? "(null)";
-}
+	public override readonly string ToString () => Value?.ToString () ?? "[null]";
 
-/// <summary>
-/// A value that may contain user-identifiable information and is
-/// intended to be converted to a string and logged to an <see cref="ILogger"/>.
-/// The logger must hash this value if PII is a concern.
-/// </summary>
-public struct UserIdentifiableString
-{
-	public string Value { get; }
-	public UserIdentifiableString (string value) { Value = value; }
-	public static implicit operator UserIdentifiableString (string value) => new (value);
-	public override readonly string ToString () => Value.ToString ();
-}
-
-/// <summary>
-/// A type to be logged to an <see cref="ILogger"/> that may originate from the user e.g. analyzer assemblies.
-/// The logger must hash this value if PII is a concern, but may avoid hashing types that are known to be part of the app itself.
-/// </summary>
-public struct UserIdentifiableType
-{
-	public Type Type { get; }
-	public UserIdentifiableType (Type type) { Type = type; }
-	public static implicit operator UserIdentifiableType (Type type) => new (type);
-	public override readonly string ToString () => Type is null? "[null]" : Type.ToString ();
+	object? IUserIdentifiableValue.Value => Value;
 }
 
 /// <summary>
 /// A filename to be logged to an <see cref="ILogger"/> that may originate from the user.
-/// The logger must hash this value if PII is a concern, but may avoid hashing filenames that are known to be part of the app itself.
+/// Loggers that strip PII must hash this value unless it can be reliably determined
+/// to be a non-identifiable value such a filename that is part of the app itself.
 /// </summary>
-public struct UserIdentifiableFileName
+public readonly struct UserIdentifiableFileName : IUserIdentifiable<string>
 {
-	public string Filename { get; }
-	public UserIdentifiableFileName (string filename) { Filename = filename; }
+	public string Value { get; }
+	public UserIdentifiableFileName (string filename) { Value = filename; }
 	public static implicit operator UserIdentifiableFileName (string filename) => new (filename);
-	public override readonly string ToString () => Filename;
+	public override readonly string ToString () => Value;
+
+	object? IUserIdentifiableValue.Value => Value;
 }
